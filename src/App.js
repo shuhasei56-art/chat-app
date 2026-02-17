33dcd66b0ce045a6cdf9cfc01fcad05c0a28709a
shuhasei56-art <shuhasei56@gmail.com>
2026-02-17 17:02:13 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5937,7 +5937,7 @@ const PachinkoView = ({ user, profile, onBack, showNotification }) => {
   const QUOTA_BACKOFF_MS = 30_000; // クォータ超過時の待機
 
   const COST = 100;
-  const WIN = 500;
+  const WIN = 1000;
   const PROB_MIN = 1 / 80;
   const PROB_MAX = 1 / 30;
 
